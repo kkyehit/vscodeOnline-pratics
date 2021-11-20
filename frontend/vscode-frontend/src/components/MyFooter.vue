7123b5aabf482079c7b093897aedf6a4b8b6cdb2
kkeyhit <rp01020113994@gmail.com>
2021-11-21 02:26:16 +0900

    footer예약어 경고 - name변경 (Footer -> MyFooter)

---
+++ frontend/vscode-frontend/src/components/MyFooter.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="MyFooter">
+        <h2>{{ msg }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter',
+//   props: {
+//     msg: "This is Header"
+//   }
+  data() {
+      return {
+          msg: "This is footer"
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
